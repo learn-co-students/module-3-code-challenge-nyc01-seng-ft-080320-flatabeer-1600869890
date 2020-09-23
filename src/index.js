df0e1dcadb8a05d7ef0a8a8ab702c54a795e0770
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-09-23 10:40:01 -0400

    3/4 done with first deliverable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 // Code here
+document.addEventListener("DOMContentLoaded", event => {
+    
+    
+    const baseUrl = "http://localhost:3000/"
+
+
+    // See the first beer's details, including its name, image, description, and reviews, when the page loads
+
+    fetch("http://localhost:3000/beers/1") 
+    .then(response => response.json())
+    .then(firstBeerJson => firstBeerDetails(firstBeerJson))
+
+    const firstBeerDetails = (firstBeerJson) => {
+        console.log(firstBeerJson)
+        const beerName = firstBeerJson
+        const beerDeetsDiv = document.querySelector(".beer-details")
+            beerDeetsDiv.querySelector("h2").innerHTML = `${firstBeerJson.name}`
+            beerDeetsDiv.querySelector("img").setAttribute('src', firstBeerJson.image_url)
+            beerDeetsDiv.querySelector("form.description").querySelector("textarea").innerHTML = `${firstBeerJson.description}`
+            
+       
+        console.log(beerDeetsDiv.querySelector("h2"))
+        
+    }
+
+})
