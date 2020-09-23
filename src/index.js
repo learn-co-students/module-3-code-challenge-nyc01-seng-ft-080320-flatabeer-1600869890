6d481dba84c41a522b6d6cd43db3054b2db67f89
kevinpark07 <kevin.park07@yahoo.com>
2020-09-23 10:56:52 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,57 +1,108 @@
 const urlBeer1 = "http://localhost:3000/beers/1"
 // load the content and render all of the beer objects
-document.addEventListener("DOMContentLoaded", e => {
+    document.addEventListener("DOMContentLoaded", e => {
 
 
 
-fetchBeer(urlBeer1);
-})
-//create a fetch request (get) to get all of the beer data from api
-
-const fetchBeer = url => {
-
-    fetch(url)
-    .then(response => response.json())
-    .then(beerObj => {
-        renderBeer(beerObj);
-        renderReviews(beerObj);
+    fetchBeer(urlBeer1);
+    submitHandler();
     })
-}
-//include a function that will render each beer with the format listed below
-const renderBeer = beerObj => {
-    const beerDiv = document.createElement('div');
-    beerDiv.classList.add("beer-details");
-    beerDiv.innerHTML = `
-        <h2>${beerObj.name}</h2>
-        <img src=${beerObj.image_url}>
+    //create a fetch request (get) to get all of the beer data from api
 
-        <form class="description">
-        <textarea>${beerObj.description}</textarea>
-        <button>Update Beer</button>
-        </form>
+    const fetchBeer = url => {
 
-        <h3>Leave a Review</h3>
-        <form class="review-form">
-        <textarea></textarea>
-        <input type="submit" value="Submit">
-        </form>
+        fetch(url)
+        .then(response => response.json())
+        .then(beerObj => {
+            renderBeer(beerObj);
+            renderReviews(beerObj);
+        })
+    }
+    //include a function that will render each beer with the format listed below
+    const renderBeer = beerObj => {
+        const beerDiv = document.createElement('div');
+        beerDiv.classList.add("beer-details");
+        beerDiv.innerHTML = `
+            <h2>${beerObj.name}</h2>
+            <img src=${beerObj.image_url}>
 
-        <h3>Customer Reviews</h3>
-        <ul class="reviews">
-        </ul>
-    `;
-    
-    const main = document.querySelector('main');
-    main.append(beerDiv);
-}
+            <form class="description" data-beer-id=${beerObj.id}>
+            <textarea name="description">${beerObj.description}</textarea>
+            <button>Update Beer</button>
+            </form>
 
-//render the reviews indivually using the same get request
-const renderReviews = beerObj => {
-    const reviewLis = document.createElement('li');
-    const beerReviews = beerObj.reviews;
-    for(const beer of beerReviews)
-    console.log(beerReviews);
-}
+            <h3>Leave a Review</h3>
+            <form class="review-form" data-beer-id=${beerObj.id}>
+            <textarea name="review"></textarea>
+            <input type="submit" value="Submit">
+            </form>
+
+            <h3>Customer Reviews</h3>
+            <ul class="reviews">
+            </ul>
+        `;
+        
+        const main = document.querySelector('main');
+        main.append(beerDiv);
+    }
+
+    //render the reviews indivually using the same get request
+    const renderReviews = beerObj => {
+        const reviewList = document.querySelector(".reviews")
+        const beerReviews = beerObj.reviews;
+        for(const review of beerReviews) {
+            const reviewLis = document.createElement('li');
+            reviewLis.textContent = review;
+            reviewList.append(reviewLis);
+        }
+    }
+
+    //submit handler for the description update
+    const submitHandler = () => {
+        document.addEventListener('submit', e => {
+            e.preventDefault();
+            if (e.target.matches('.description')) {
+                updateDescription(e.target);
+            }
+        })
+    }
+    //function to send the patch request
+    const updateDescription = form => {
+        //const beerId = form.dataset.beerId;
+        const description = form.description.value;
+        console.dir(description);
+
+        const options = {
+            method: "PATCH",
+            headers: {
+                "content-type": "application/json",
+                "accept": "application/json"
+            },
+            body: JSON.stringify(description)
+        }
+
+        fetch(urlBeer1, options)
+        .then(response => response.json())
+        .then()
+    }
+    //remove the old description on DOM and re-render the new description
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //<li>Replace with actual reviews</li>
