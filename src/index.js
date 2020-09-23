83a3845adb1219d4ef47b8e5f5ccff104a7f44d6
Mar5Planet <amustaf@email.arizona.edu>
2020-09-23 11:14:36 -0400

    persistant reviews added

--- src/index.js
+++ src/index.js
@@ -31,6 +31,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     
     }
 
+    const patchReview = (beerId, reviewArr) => {
+        const options = {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                "reviews": reviewArr
+            })
+        }
+
+        fetch(baseUrl + beerId, options)
+        .then(res => res.json())
+        // .then(beer => renderBeer(beer))
+        .catch(error => console.log(error))
+            
+                    
+    }
+
+
     const renderBeer = beerObj => {
         const beerDiv = document.querySelector('.beer-details')
         const beerNameHeader = beerDiv.children[0]
@@ -55,9 +76,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const reviewsUl = document.querySelector(".reviews")
         const reviewLi = document.createElement("li")
         reviewLi.textContent = newReview
-        console.log(reviewLi)
-        console.log(reviewsUl)
+       
         reviewsUl.insertBefore(reviewLi, reviewsUl.firstChild)
+        let reviewArr = []
+        const reviewChildren = reviewsUl.children
+        for (let i =0; i < reviewChildren.length; i++) {
+            reviewArr.push(reviewChildren[i].textContent)
+           
+        }
+       
+        return reviewArr
     }
 
 
@@ -74,8 +102,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
         if (e.target.matches(".review-form")) {
             
             const reviewText = e.target.children[0].value
+            const beerId = document.querySelector('.beer-details').children[0].getAttribute('data-id')
+            let reviewArr = addReview(reviewText)
+            
+            patchReview(beerId, reviewArr)
 
-            addReview(reviewText)
+            e.target.reset()
         }
     })
 
