2349d5a5813a065408a4b7559d2996c83f57b523
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-09-23 10:54:54 -0400

    completed core deliverables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Code here
 document.addEventListener("DOMContentLoaded", () => {
-    const url = "http://localhost:3000/beers"
+    const url = "http://localhost:3000/beers/"
     const getAndDisplayBeer = () => {
         fetch("http://localhost:3000/beers/1")
         .then(response => response.json())
@@ -20,8 +20,8 @@ document.addEventListener("DOMContentLoaded", () => {
         <img src="${beer.image_url}">
 
         <form class="description">
-          <textarea>${beer.description}</textarea>
-          <button>Update Beer</button>
+          <textarea class="beer-description">${beer.description}</textarea>
+          <button class="update-beer">Update Beer</button>
         </form>
 
         <h3>Leave a Review</h3>
@@ -52,7 +52,63 @@ document.addEventListener("DOMContentLoaded", () => {
 
     }
 
+    const clickHandler= () => {
+        document.addEventListener('click', e => {
+            if(e.target.matches('.update-beer')){
+                e.preventDefault()
+                updateReview(e.target)
+            }
+        })
 
+    }
 
+    const submitHandler = () => {
+        document.addEventListener('submit', e => {
+            if(e.target.matches('.review-form')){
+                e.preventDefault()
+                const form = e.target
+                const review = form.querySelector('textarea')
+                const newReviewLi = document.createElement('li')
+                newReviewLi.innerText = review.value
+                const reviewUl = document.querySelector('.reviews')
+                reviewUl.append(newReviewLi)
+                //take contents from form and create new li and append to reviews ul
+
+            }
+
+        })
+       
+
+    }
+    const updateReview = el => {
+        const beerId = el.parentElement.parentElement.dataset.beerId
+
+        //get new review and update beer.reveiew
+        
+        const textArea = el.parentElement.querySelector('textarea')
+        const beerObj = {
+            description: textArea.value
+        }
+        const options = {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(beerObj)
+        }
+        fetch(url+beerId, options)
+        .then(response => response.json())
+        .then(beer => {
+            const beerDiv = el.parentElement.parentElement
+            const descriptionArea = beerDiv.querySelector('.beer-description')
+            descriptionArea.innerText = beer.description
+        })
+    }
+
+    
+
+    clickHandler()
+    submitHandler()
     getAndDisplayBeer()
 })
