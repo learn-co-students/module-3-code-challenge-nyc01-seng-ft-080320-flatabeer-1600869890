f41060064e37720f8996258bb5cc7168c7d57a94
Lazarus22 <matthew.peters.nyc@gmail.com>
2020-09-23 10:37:43 -0400

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // Code here
+document.addEventListener('DOMContentLoaded', e => {
+
+    const qs = (selector) => document.querySelector(selector)
+    const ce = (element) => document.createElement(element)
+
+    const URL = 'http://localhost:3000/beers/'
+    const URL1 = 'http://localhost:3000/beers/1'
+
+    fetch(URL1)
+        .then(resp => resp.json())
+        .then(json => renderBeer(json))
+
+    const renderBeer = (beer) => {
+        const info = qs('.beer-details')
+        const beerName = ce('div')
+        
+    }
+
+})//dom content loaded
