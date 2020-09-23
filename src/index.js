154dcd48ac3b8235b811333b969931a69e8b7c80
stackguardian <mferdous98@aol.com>
2020-09-23 10:56:44 -0400

    message

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', e => {
     const baseUrl = 'http://localhost:3000/beers/'
-    const updateButton = document.querySelector('.description button')
-    updateButton.classList.add('update')
 
     const getFirstBeer = () => {
         fetch(baseUrl + 1)
@@ -21,8 +19,35 @@ document.addEventListener('DOMContentLoaded', e => {
         document.addEventListener('submit', e => {
             e.preventDefault()
             if(e.target.matches('.description')){
-                
+                const form = e.target
+                const updateDescription = form[0].value 
+
+                const options = {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        accept: 'application/json'
+                    },
+                    body: JSON.stringify({description: updateDescription})
+                }
+                fetch(baseUrl + 1, options)
+                .then(response => response.json())
+                .then(updatedDescription => {
+                    document.querySelector('textarea').textContent = updatedDescription.description
+                })
             }
+            if(e.target.matches('.review-form')){
+                const customerReviews = document.querySelector('.reviews li')
+
+                const reviewForm = e.target
+                
+                const reviewText = reviewForm[0].value
+                const reviewLi = document.createElement('li')
+                reviewLi.innerText = reviewText
+
+                customerReviews.append(reviewLi)
+            }
+            
         })
     }
 
@@ -52,14 +77,14 @@ document.addEventListener('DOMContentLoaded', e => {
 
 // 1). See the first beer's details, including its **name, image, description, and reviews**, when the page loads
 
-    // - Get beer data
-    // - Add beer info
+    // √- Get beer data
+    // √- Add beer info
 
 // 2). Change the beer's description and **still see that change when reloading the page**
 
-    // - Listen for submit event
-    // - Edit beer description
+    // √- Listen for submit event
+    // √- Edit beer description
 
 // 3). Add a review for the beer (no persistence needed)
 
-    // - Add a review
+    // √- Add a review
