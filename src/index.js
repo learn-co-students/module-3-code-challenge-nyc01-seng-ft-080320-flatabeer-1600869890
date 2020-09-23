98857f067020df980dbd6893f0d1b6673db6a5c6
Mattie43 <ericksenm93@ymail.com>
2020-09-23 10:26:30 -0400

    1/3 deliverables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 // Code here
+// name, image, description, and reviews
+document.addEventListener('DOMContentLoaded', () =>{
+  function getBeer(beerId) {
+    fetch(`http://localhost:3000/beers/${beerId}`)
+      .then(resp => resp.json())
+      .then(data => displayBeer(data))
+  }
+
+  function displayBeer(beerObj) {
+    const beerDetails = document.querySelector('.beer-details')
+    beerDetails.firstElementChild.innerText = beerObj.name
+    beerDetails.querySelector('img').src = beerObj.image_url
+    beerDetails.querySelector('.description').querySelector('textarea').innerText = beerObj.description
+
+    for (const review of beerObj.reviews) {
+      displayReview(review)
+    }
+  }
+
+  function displayReview(review) {
+    const newLi = document.createElement('li')
+    newLi.innerText = review
+    document.querySelector('.reviews').append(newLi)
+  }
+  
+  getBeer(1)
+})
