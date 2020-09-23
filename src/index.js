ac960cf8760590c8b2e3d7edfcf20c69dd395c69
alerner1 <alyssplerner@gmail.com>
2020-09-23 10:22:19 -0400

    rendered beer except reviews

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-// Code here
+document.addEventListener('DOMContentLoaded', e => {
+  const baseURL = 'http:localhost:3000/beers';
+
+  const fetchBeer = (beerId) => {
+    fetch(`${baseURL}/${beerId}`)
+      .then(resp => resp.json())
+      .then(json => renderBeer(json));
+  };
+
+  const renderBeer = (beer) => {
+    const deetsDiv = document.querySelector('.beer-details');
+    
+    const nameHeader = deetsDiv.querySelector('h2');
+    nameHeader.textContent = beer.name;
+
+    const beerImg = deetsDiv.querySelector('img');
+    beerImg.src = beer['image_url'];
+
+    const descForm = deetsDiv.querySelector('.description');
+    const descBox = descForm.querySelector('textarea');
+    descBox.value = beer.description;    
+
+    const reviewUl = deetsDiv.querySelector('.reviews');
+    
+    removeReviews(reviewUl);
+
+    for (let review of beer.reviews) {
+      renderReview(reviewUl, review);
+    }
+  };
+
+  const removeReviews = (reviewUl) => {
+    while (reviewUl.firstElementChild) {
+      reviewUl.firstElementChild.remove();
+    }
+  };
+
+  // const renderReview = (reviewUl, review) => {
+  //   const newReview = document.createElement()
+  // };
+
+  fetchBeer(1);
+
+});
