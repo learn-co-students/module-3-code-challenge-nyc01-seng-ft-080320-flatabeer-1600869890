5ceb3883fdcfb1fb71b15e0dde65384c8ee04b44
vnapuri <vladnapuri@gmail.com>
2020-09-23 10:37:02 -0400

    first Commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 // Code here
+document.addEventListener("DOMContentLoaded", e => {
+
+
+
+const getBeers = () => {
+    
+    fetch("http://localhost:3000")
+    .then(response => response.json())
+    .then(beersArray => console.log(beersArray))
+
+}
+
+
+
+
+getBeers()
+})
+
