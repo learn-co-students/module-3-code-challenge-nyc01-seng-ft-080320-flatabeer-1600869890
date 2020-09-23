44a436e07623878abe82f913e59be20104d98d7e
zhaoerik <jediofthenyy@gmail.com>
2020-09-23 11:42:50 -0400

    not finished

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     baseUrl = 'http://localhost:3000/beers'
 
-    const fetchbeers = () => {
+    const fetchBeers = () => {
         fetch(baseUrl)
         .then(res => res.json())
         .then(beers => renderBeers(beers))
@@ -16,13 +16,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const renderBeer = beer => {
         const beerDetailsDiv = document.querySelector('.beer-details')
+        beerDetailsDiv.dataset.id = beer.id
         beerDetailsDiv.innerHTML = `
         <h2>${beer.name}</h2>
         <img src=${beer.image_url}>
 
         <form class="description">
           <textarea>${beer.description}</textarea>
-          <button>Update Beer</button>
+          <button class="update-button">Update Beer</button>
         </form>
 
         <h3>Leave a Review</h3>
@@ -37,6 +38,37 @@ document.addEventListener('DOMContentLoaded', () => {
         </ul>
         `
     }
+    
+    const clickHandler = () => {
+        document.addEventListener('click', e => {
+            e.preventDefault()
+            if (e.target.matches(".update-button")){
+                const textArea = document.querySelector('textarea')
+                const newDesc = textArea.textContent
+                const beerDetailsDiv = document.querySelector('.beer-details')
+                const beerId = beerDetailsDiv.dataset.id
 
-    fetchbeers()
+
+                
+                let options = {
+                    method: "PATCH",
+                    headers: {
+                        'content-type': 'application/json',
+                        'accept': 'application/json'
+                    },
+                    body: JSON.stringify(newDesc)
+                }
+            
+                fetch(baseUrl + beerId, options)
+                .then(res => res.json())
+                .then(fetchBeers)
+                
+            }
+        })
+    }
+
+
+
+    fetchBeers()
+    clickHandler()
 })
