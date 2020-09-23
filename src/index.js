3af06db0e6133302b1a48386a99e41a4b92c3634
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-09-23 10:58:52 -0400

    first deliverable done

--- src/index.js
+++ src/index.js
@@ -12,15 +12,25 @@ document.addEventListener("DOMContentLoaded", event => {
     .then(firstBeerJson => firstBeerDetails(firstBeerJson))
 
     const firstBeerDetails = (firstBeerJson) => {
-        console.log(firstBeerJson)
+        console.log("firstBeerJson:", firstBeerJson["reviews"])
         const beerName = firstBeerJson
         const beerDeetsDiv = document.querySelector(".beer-details")
             beerDeetsDiv.querySelector("h2").innerHTML = `${firstBeerJson.name}`
             beerDeetsDiv.querySelector("img").setAttribute('src', firstBeerJson.image_url)
             beerDeetsDiv.querySelector("form.description").querySelector("textarea").innerHTML = `${firstBeerJson.description}`
+        const beerReviews = firstBeerJson["reviews"]
+        const beerReviewsDiv = document.querySelector(".reviews")
+            beerReviewsDiv.innerHTML =  ` `
+
+    //    const beerReviews = document.querySelector(".reviews") => use in case above doesnt work
+        beerReviews.forEach(element => {
+            const reviewLi = document.createElement("li")
+            reviewLi.innerHTML = ` <p>${element}</p>`
+
+            beerReviewsDiv.appendChild(reviewLi)
+
             
-       
-        console.log(beerDeetsDiv.querySelector("h2"))
+        });
         
     }
 
