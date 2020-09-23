cf032edc1a25ac961d2100a8dcd8e3fb0df109f6
github username <github email address>
2020-09-23 10:37:09 -0400

    First commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 // Code here
+
+// Render beer:
+
+document.addEventListener("DOMContentLoaded", function(e){
+
+    const renderBeer = (beerObj) =>{
+        const beerContainer = document.querySelector('.beer-details')
+        // beerContainer.dataset.id = "1"
+        const header = beerContainer.querySelector('h2').textContent = `${beerObj.name}`
+        const image = beerContainer.querySelector('img')
+        image.innerHTML =  `<img src=${beerObj.image_url}>`
+        const description = beerContainer.querySelector('form').firstElementChild.textContent = `${beerObj.description}`
+    }
+
+    const getBeer = () =>{
+        const beerContainer = document.querySelector('.beer-details')
+        beerContainer.dataset.id = "1"
+        const id = beerContainer.dataset.id
+        console.log(id)
+        
+        fetch('http://localhost:3000/beers/' + id)
+        .then(response => response.json())
+        .then(beer => renderBeer(beer))
+    }
+
+    //Function Invocations
+    // renderBeer()
+    getBeer()
+
+})
+
+
+
