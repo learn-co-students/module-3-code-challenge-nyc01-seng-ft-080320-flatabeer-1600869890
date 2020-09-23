c4c941aff027c6d85745770c608a6023610be6ad
Lazarus22 <matthew.peters.nyc@gmail.com>
2020-09-23 11:15:55 -0400

    3

--- src/index.js
+++ src/index.js
@@ -17,6 +17,52 @@ document.addEventListener('DOMContentLoaded', e => {
         document.getElementById("image").src=beer.image_url
         name.textContent = beer.name
         description.textContent = beer.description
-        image.scr = beer.img_url
+        ul = ce('ul')
+        const reviews = qs('#reviews')
+        reviews.innerHTML = ""
+        reviews.appendChild(ul)
+        beer.reviews.forEach(function (item) {
+            let li = document.createElement('li');
+            ul.appendChild(li);
+            li.innerHTML += item;
+        })
     }
+
+    const clickHandler = () => {
+        document.addEventListener('click', e => {
+            e.preventDefault()
+            if (e.target.matches('button')) {
+                const descriptionForm = e.target
+                patchBeer(descriptionForm)
+        
+            } else if (e.target.matches('#new-object-form')) {
+                const newForm = e.target
+                postObject(newForm)
+            }
+        })}
+
+        const patchBeer = (form, reset = false) => {
+            let id = form.id.value
+            let newBeer = parseInt(form.beer.value)
+        
+            const newBeer = {
+                "description": form.description.value,
+            }
+        
+            const options = {
+                method: 'PATCH',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newBeer)
+            }
+        
+            fetch(URL1, options)
+                .then(resp => resp.json())
+                .then(renderBeer)
+        }
+
+
+
+        clickHandler()
 })//dom content loaded
