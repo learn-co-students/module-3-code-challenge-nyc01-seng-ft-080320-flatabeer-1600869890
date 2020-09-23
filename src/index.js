4dd022eac13f8a4e8a63c5a1766329a33c62644b
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-09-23 11:15:43 -0400

    embarassing

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
-document.addEventListener('click', e => {
+document.addEventListener("DOMContentLoaded", e => {
 
-const beerDetails = document.querySelector('.beer-details')
-
+     const beerDetailsHeader = document.querySelector('.beer-details')
+     const beerForm = document.querySelector('.description')
 
 
 function fetchFirstBeer() {
      fetch("http://localhost:3000/beers/1")
      .then(resp => resp.json())
-     .then(beer => {
-          console.log(beer)
-          renderBeer(beer)   
+     .then(beer => { 
+          renderBeerHeader(beer)
+          renderBeerForm()
+
      })
+     }
+
+
+
+function renderBeerHeader(beer){
+     beerDetailsHeader.innerHTML = 
+     ` <h2>${beer.name}</h2>
+     <img src="${beer.image_url}"> '
 }
 
-function renderBeer(beer){
-     beerDetails.innerHTML = 
-
-
-
+function renderBeerForm(beer){
+     beerForm.innerHTML = `
+          <form>
+          <textarea> ${beer.description}</textarea>
+          <button>Update Beer</button>
+        </form> `
 }
-
-
-
 
 
 
