377a50b20a10106aa301ac987542cfecd2ddaea8
alerner1 <alyssplerner@gmail.com>
2020-09-23 11:45:05 -0400

    added comments

--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ document.addEventListener('DOMContentLoaded', e => {
     "Accept": "application/json"
   };
 
+  // fetch all beers with GET request (for sidebar)
   const fetchBeerList = () => {
     fetch(baseURL)
       .then(resp => resp.json())
       .then(json => renderBeerList(json));
   };
 
+  // add beers to sidebar
   const renderBeerList = (beers) => {
     removeChildren(beerListUl);
     for (let beer of beers) {
@@ -21,6 +23,7 @@ document.addEventListener('DOMContentLoaded', e => {
     }
   };
 
+  // for each beer, create li in sidebar with name and beer ID
   const renderBeerName = (beer) => {
     const newBeerName = document.createElement('li');
     newBeerName.dataset.beerId = beer.id;
@@ -29,12 +32,14 @@ document.addEventListener('DOMContentLoaded', e => {
     beerListUl.append(newBeerName);
   };
 
+  // fetch first beer to be the default rendered when the page is loaded
   const fetchBeer = (beerId) => {
     fetch(`${baseURL}/${beerId}`)
       .then(resp => resp.json())
       .then(json => renderBeer(json));
   };
 
+  // create html elements for beer with appropriate info
   const renderBeer = (beer) => {
     const deetsDiv = document.querySelector('.beer-details');
     deetsDiv.dataset.id = beer.id;
@@ -57,12 +62,15 @@ document.addEventListener('DOMContentLoaded', e => {
     }
   };
 
+  // create html elements for each review
   const renderReview = (i, review) => {
     const newReview = document.createElement('li');
     newReview.textContent = review;
 
     const deleteButton = document.createElement('button');
     deleteButton.className = 'delete';
+
+    // so we know which review this delete button is tied to
     deleteButton.dataset.reviewIndex = i;
     deleteButton.textContent = 'Delete';    
 
@@ -71,6 +79,7 @@ document.addEventListener('DOMContentLoaded', e => {
 
   };
 
+  // get beer description from textarea and patch accordingly
   const updateBeerDesc = () => {
     const editForm = document.querySelector('.description');
     const beerId = parseInt(editForm.parentElement.dataset.id, 10);
@@ -87,6 +96,7 @@ document.addEventListener('DOMContentLoaded', e => {
       .then(json => renderBeer(json));
   };
 
+  // get new review from textarea, add to reviews array, and patch accordingly
   const addBeerReview = () => {
     const newReview = reviewForm.querySelector('textarea').value;
     const reviews = getReviews();
@@ -97,6 +107,7 @@ document.addEventListener('DOMContentLoaded', e => {
     patchReviews(reviews);
   };
 
+  // remove review from reviews array and patch accordingly
   const deleteReview = (reviewIndex) => {
     // using patch here, not delete, since we're updating an entry rather than deleting one
     // if the api included a review model, I'd use a delete request to the appropriate review
@@ -106,17 +117,21 @@ document.addEventListener('DOMContentLoaded', e => {
     patchReviews(reviews);
   }
 
+  // remove child elements from parent
   const removeChildren = (parent) => {
     while (parent.firstElementChild) {
       parent.firstElementChild.remove();
     }
   };
 
+  // create array of text of each review
   const getReviews = () => {
     const reviewsLis = Array.from(reviewUl.querySelectorAll('li'));
     return reviewsLis.map(li => li.textContent);
   };
 
+  // update reviews using patch request to api, with array of reviews passed in 
+  // as param from the function that calls it
   const patchReviews = (reviews) => {
     const beerId = parseInt(reviewForm.parentElement.dataset.id, 10);
 
@@ -132,6 +147,7 @@ document.addEventListener('DOMContentLoaded', e => {
   };
 
   const clickHandler = () => {
+    // listens for clicks to update description, delete review, or render a different beer from sidebar
     document.addEventListener('click', e => {
       if (e.target.matches('button')) {
         e.preventDefault();
@@ -145,6 +161,7 @@ document.addEventListener('DOMContentLoaded', e => {
       }
     });
 
+    // listens for submit to add new review
     document.addEventListener('submit', e => {
       e.preventDefault();
       addBeerReview();
