923cda9235664c57ea1476da9b515e2e893fdf1c
kevinpark07 <kevin.park07@yahoo.com>
2020-09-23 11:36:43 -0400

    fourth commit

--- src/index.js
+++ src/index.js
@@ -50,9 +50,9 @@ const urlBeer1 = "http://localhost:3000/beers/1"
         const reviewList = document.querySelector(".reviews")
         const beerReviews = beerObj.reviews;
         for(const review of beerReviews) {
-            const reviewLis = document.createElement('li');
-            reviewLis.textContent = review;
-            reviewList.append(reviewLis);
+            const reviewLi = document.createElement('li');
+            reviewLi.textContent = review;
+            reviewList.append(reviewLi);
         }
     }
     
@@ -99,9 +99,48 @@ const urlBeer1 = "http://localhost:3000/beers/1"
         const main = document.querySelector('main');
         main.innerHTML = ""
     }
-
+    
     //function to post a new review on DOM
     //no persistence required
+    // const postNewReview = form => {
+        
+    //     const newReview = form.review.value;
+
+    //     const reviewList = document.querySelector(".reviews");
+    //     const reviewLi = document.createElement('li');
+    //     reviewLi.textContent = newReview;
+
+    //     reviewList.prepend(reviewLi);
+
+    // }
+
+    //Review Persistence Attmept
+    const postNewReview = form => {
+        const currentReviews = []
+        const newReview = form.review.value;
+        const beerDiv = form.parentElement;
+        const reviewUl = beerDiv.querySelector('ul');
+        const reviewLis = reviewUl.children
+        for (const review of reviewLis) {
+            currentReviews.push(review.textContent);
+        }
+        
+        currentReviews.unshift(newReview);
+
+        //console.log(currentReviews);
+        const options = {
+            method: "PATCH",
+            headers: {
+                "content-type": "application/json",
+                "accept": "application/json"
+            },
+            body: JSON.stringify({
+                reviews: currentReviews
+            })
+        }
+        fetch
+    }
+
 
 
 
