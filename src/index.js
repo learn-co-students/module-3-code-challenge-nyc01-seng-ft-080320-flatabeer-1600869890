6564b3638db6fb4f8472cae68e45c26f3846410e
Mattie43 <ericksenm93@ymail.com>
2020-09-23 10:42:02 -0400

    2/3 deliverables

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () =>{
   function displayBeer(beerObj) {
     const beerDetails = document.querySelector('.beer-details')
     beerDetails.firstElementChild.innerText = beerObj.name
+    beerDetails.id = beerObj.id
     beerDetails.querySelector('img').src = beerObj.image_url
     beerDetails.querySelector('.description').querySelector('textarea').innerText = beerObj.description
 
@@ -22,6 +23,30 @@ document.addEventListener('DOMContentLoaded', () =>{
     const newLi = document.createElement('li')
     newLi.innerText = review
     document.querySelector('.reviews').append(newLi)
+  }
+
+  document.addEventListener('submit', e => {
+    e.preventDefault()
+    if(e.target.className == 'description'){
+      updateBeerDesc(e.target.parentNode.id, e.target.firstElementChild.value)
+    }else if(e.target.className == 'review-form'){
+      console.log(e.target.className)
+    }
+  })
+  function updateBeerDesc(beerId, desc) {
+    console.log(beerId, desc)
+    const options = {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      },
+      body: JSON.stringify({
+        description: desc
+      })
+    }
+    
+    fetch(`http://localhost:3000/beers/${beerId}`, options)
   }
   
   getBeer(1)
