ca3dc10f30d8eefc2be27d569a7496e666f61e62
soroosh-gb <soroosh.gb@gmail.com>
2020-09-23 10:39:37 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 // Code here
+document.addEventListener('DOMContentLoaded', () => {
+    getFirstBeer()
+})
+
+const getFirstBeer = () => {
+    fetch("http://localhost:3000/beers/1")
+    .then(response => response.json())
+    .then(beer => render(beer))
+}
+
+const render = beer =>{
+    console.log(beer)
+    let name = document.getElementsByTagName('h2')
+    name[0].innerText = beer.name
+
+    let img = document.getElementsByTagName('img')
+    img[0].src = beer.image_url
+    
+    const form = document.querySelector(".description")
+    form.textContent = beer.description 
+
+    
+}
