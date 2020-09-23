aac4057816c200a961162605d9e8a4de68ad692b
alerner1 <alyssplerner@gmail.com>
2020-09-23 10:34:52 -0400

    core deliverable #1-2 complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', e => {
   const baseURL = 'http:localhost:3000/beers';
+  const headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  };
 
   const fetchBeer = (beerId) => {
     fetch(`${baseURL}/${beerId}`)
@@ -9,6 +13,7 @@ document.addEventListener('DOMContentLoaded', e => {
 
   const renderBeer = (beer) => {
     const deetsDiv = document.querySelector('.beer-details');
+    deetsDiv.dataset.id = beer.id;
     
     const nameHeader = deetsDiv.querySelector('h2');
     nameHeader.textContent = beer.name;
@@ -42,6 +47,34 @@ document.addEventListener('DOMContentLoaded', e => {
     reviewUl.append(newReview);
   };
 
+  const updateBeerDesc = () => {
+    const editForm = document.querySelector('.description');
+
+    const beerId = parseInt(editForm.parentElement.dataset.id, 10);
+    const editedDesc = editForm.querySelector('textarea').value;
+
+    fetch(`${baseURL}/${beerId}`, {
+      method: "PATCH",
+      headers: headers,
+      body: JSON.stringify({
+        description: editedDesc
+      })
+    })
+      .then(resp => resp.json())
+      .then(json => renderBeer(json));
+  };
+
+  const clickHandler = () => {
+    document.addEventListener('click', e => {
+      if (e.target.matches('button')) {
+        e.preventDefault();
+
+        updateBeerDesc();
+      }
+    });
+  };
+
   fetchBeer(1);
+  clickHandler();
 
 });
