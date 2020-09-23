1cd6410e02d5b03df4d9e13173e77cb4aa808e51
Mattie43 <ericksenm93@ymail.com>
2020-09-23 11:44:42 -0400

    2/3 advanced deliverables done

--- src/index.js
+++ src/index.js
@@ -14,15 +14,20 @@ document.addEventListener('DOMContentLoaded', () =>{
     beerDetails.querySelector('img').src = beerObj.image_url
     beerDetails.querySelector('.description').querySelector('textarea').innerText = beerObj.description
 
-    for (const review of beerObj.reviews) {
-      displayReview(review)
+    for (let i = 0; i < beerObj.reviews.length; i++) {
+      displayReview(beerObj.reviews[i], i)
     }
   }
 
-  function displayReview(review) {
+  function displayReview(review, index) {
     const newLi = document.createElement('li')
     newLi.innerText = review
+    newLi.id = index
     document.querySelector('.reviews').append(newLi)
+    const button = document.createElement('button')
+    button.className = 'btn-delete'
+    button.innerText = 'DELETE'
+    newLi.append(button)
   }
 
   document.addEventListener('submit', e => {
@@ -32,6 +37,20 @@ document.addEventListener('DOMContentLoaded', () =>{
     }else if(e.target.className == 'review-form'){
       addReview(e.target.parentNode.id, e.target.firstElementChild.value)
       e.target.reset()
+    }
+  })
+
+  document.querySelector('.reviews').addEventListener('click', e => {
+    if(e.target.className == 'btn-delete'){
+      const array = e.target.parentNode.parentNode.querySelectorAll('li')
+      for (let i = 0; i < array.length; i++) {
+        const element = array[i]
+        if(element == e.target.parentNode){
+          getReviews(e.target.parentNode.parentNode.parentNode.id, undefined, i)
+          e.target.parentNode.remove()
+          break
+        }
+      }
     }
   })
 
@@ -50,14 +69,18 @@ document.addEventListener('DOMContentLoaded', () =>{
     fetch(`http://localhost:3000/beers/${beerId}`, options)
   }
 
-  function getReviews(beerId, newReview) {
+  function getReviews(beerId, newReview, reviewId) {
     fetch(`http://localhost:3000/beers/${beerId}`)
       .then(resp => resp.json())
-      .then(data => updateReview(beerId, data.reviews, newReview))
+      .then(data => updateReview(beerId, data.reviews, newReview, reviewId))
   }
 
-  function updateReview(beerId, reviews, newReview) {
-    reviews.push(newReview)
+  function updateReview(beerId, reviews, newReview, reviewId) {
+    if(newReview){
+      reviews.push(newReview)
+    }else if (reviewId){
+      reviews.splice(reviewId, 1)
+    }
     const options = {
       method: 'PATCH',
       headers: {
@@ -72,9 +95,10 @@ document.addEventListener('DOMContentLoaded', () =>{
   }
 
   function addReview(beerId, newReview) {
-    displayReview(newReview)
+    const index = document.querySelector('.reviews').lastChild.id
+    displayReview(newReview, parseInt(index)+1)
     getReviews(beerId, newReview)
   }
   
-  getBeer(3)
+  getBeer(2)
 })
